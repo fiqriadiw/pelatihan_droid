9e09759bdbeed63d73bade5064bd93fcb8415365
MohammadEko <mkartrianto@gmail.com>
2017-01-31 10:27:07 +0700

    Class Java Init

---
+++ classjava/app/src/main/java/ekomohammad/classjava/MainActivity.java
@@ -0,0 +1,42 @@
+package ekomohammad.classjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public String showString;
+    public TextView showText;
+    public Barang [] arrBarang = new Barang[10];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showText = (TextView) findViewById(R.id.show_text);
+
+        showString = "Manipulasi Class Java Android";
+        addSeparator();
+
+        initBarang();
+        showString += arrBarang[5].toString();
+        addSeparator();
+        showText.setText(showString);
+    }
+    public void addSeparator(){
+        showString += "\n------------------------\n";
+    }
+
+    public void initBarang(){
+        arrBarang[0] = new Barang("Laptop", 1, 7000000);
+        arrBarang[1] = new Barang("Printer", Barang.ELEKTRONIK, 500000);
+        arrBarang[2] = new Barang("Monitor", Barang.ELEKTRONIK, 300000);
+        arrBarang[3] = new Barang("Pentil", Barang.NON_ELEKTRONIK, 10000);
+        arrBarang[4] = new Barang("Klep", Barang.NON_ELEKTRONIK, 1500000);
+        arrBarang[5] = new Barang("Ruji", Barang.NON_ELEKTRONIK, 250000);
+        arrBarang[6] = new Barang("Tromol", Barang.NON_ELEKTRONIK, 50000);
+        arrBarang[7] = new Barang("Velg", Barang.NON_ELEKTRONIK, 800000);
+        arrBarang[8] = new Barang("Ban", Barang.NON_ELEKTRONIK, 350000);
+        arrBarang[9] = new Barang("Oli", Barang.NON_ELEKTRONIK, 120000);
+    }
+}
